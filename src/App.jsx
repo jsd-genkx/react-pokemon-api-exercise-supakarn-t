5f16d5f0fabddde5693496820f6d137100780f83
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-06-06 17:46:52 +0700

    change layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 // import PokemonFetch from "./components/PokemonFetch";
 import PokemonBasicFetchAxios from "./components/PokemonBasicFetchAxios";
 import PokemonFetchAxios from "./components/PokemonFetchAxios";
-// import PokemonSearchFetch from "./components/PokemonSearchFetch";
+import PokemonSearchFetch from "./components/PokemonSearchFetch";
 
 const App = () => {
 	return (
-		<div className="flex flex-col gap-8 min-h-screen p-4">
+		<div className="flex flex-col items-center gap-8 min-h-screen p-4">
 			<h1>Pokémon List</h1>
 
 			{/* Fetch API */}
@@ -15,9 +15,14 @@ const App = () => {
 
 			{/* Axios */}
 			<PokemonBasicFetchAxios />
-			<PokemonFetchAxios />
-			{/* Bonus Code Example */}
-			{/* <PokemonSearchFetch /> */}
+
+			<div className="flex items-start gap-16 w-11/12">
+				{/* Bonus Code Example */}
+				<PokemonSearchFetch />
+
+				<PokemonFetchAxios />
+			</div>
+
 			{/* Bonus Code Challenge: refactor PokemonSearchFetch to use Axios */}
 		</div>
 	);
--- src/components/PokemonFetchAxios.jsx
+++ src/components/PokemonFetchAxios.jsx
@@ -55,7 +55,8 @@ const PokemonFetchAxios = () => {
 	}, []);
 
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 rounded-xl">
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8 rounded-xl w-2/3">
+			<h2 className="text-4xl font-bold mb-6">Pokemon Card</h2>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-6xl">
 				{pokemonList.map((pokemon) => (
 					<div key={pokemon.id} className="bg-white rounded-xl shadow-md p-4">
